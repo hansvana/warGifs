e2542330f2d07d74385a4cbedb809909b645e6bb
hansvana <hansvanarken@gmail.com>
2016-04-09 15:40:26 +0200

    more code, yay!

--- script.js
+++ script.js
@@ -10,27 +10,28 @@ var Engine = {
         document.getElementById("powerbutton").onclick = function() {
             if (self.state === "START"){
                 self.lightsOn();
+                self.newPaper("page1");
             }
-        };
-
-        document.getElementById("button1").onclick = function() {
-            self.newPaper("test.html");
         };
     },
     lightsOn: function() {
         document.getElementById("powerbutton").style.backgroundImage = "url('img/pwr-butt-on.png')";
         document.getElementById("panel").style.backgroundImage = "url('img/plastic.jpg')";
+        document.getElementById("display").style.visibility = 'visible';
         document.getElementById("display").style.boxShadow = '-2px -2px 5px rgba(0,0,0,0.3), 2px 2px 5px rgba(255,255,255,0.6)';
         document.getElementById("side-button-container").style.visibility = 'visible';
     },
-    newPaper: function(filename) {
+    newPaper: function(page) {
+
+        $("#display").load("screens/" + story[page].screenFile);
+
         var $paper = $("#paper");
 
         $paper.clone().prop('id','newPaper').appendTo($('body'));
 
         $("#newPaper").css('top', '-500px')
 
-        $("#newPaper").load("papers/" + filename,function(){
+        $("#newPaper").load("papers/" + story[page].paperFile,function(){
             $("#newPaper").animate({top:-250}, 200);
             $("#newPaper").delay(300).animate({top:0}, 200);
             $("#newPaper").delay(300).animate({top:120}, 200);
@@ -42,7 +43,16 @@ var Engine = {
         });
 
 
-
+        var self = this;
+        document.getElementById("button1").onclick = function() {
+            self.newPaper(story[page].choiceActions[0]);
+        };
+        document.getElementById("button2").onclick = function() {
+            self.newPaper(story[page].choiceActions[1]);
+        };
+        document.getElementById("button3").onclick = function() {
+            self.newPaper(story[page].choiceActions[2]);
+        };
 
     }
 
--- story.js
+++ story.js
@@ -1,3 +1,30 @@
-/**
- * Created by hans on 09/04/16.
- */
+var story = {
+    page1: {
+      screenFile: "testscreen.html",
+      paperFile: "test.html",
+      choiceActions: [
+          "page2","page3","page4"
+      ]
+    },
+    page2: {
+        screenFile: "testscreen2.html",
+        paperFile: "test2.html",
+        choiceActions: [
+            "page1","page3","page4"
+        ]
+    },
+    page3: {
+        screenFile: "testscreen3.html",
+        paperFile: "test3.html",
+        choiceActions: [
+            "page1","page2","page4"
+        ]
+    },
+    page4: {
+        screenFile: "testscreen4.html",
+        paperFile: "test4.html",
+        choiceActions: [
+            "page1","page2","page3"
+        ]
+    }
+};
